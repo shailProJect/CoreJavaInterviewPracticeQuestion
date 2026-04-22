88255b438b65f88ca6b09e190780d71d97167b2a
Shailesh Choudhary <125347306+shailProJect@users.noreply.github.com>
2026-04-22 10:57:44 +0530

    Add files via upload

---
+++ CoreJava/src/array/Predict_the_Output.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Predict_the_Output {
+  
+   public static void predict1() {
+     int arr[] = {10, 20, 30, 40};
+     System.out.println(arr[arr.length - 1]);
+   }
+   
+  public static void main(String[] args) {
+    predict1();
+  }
+  
+}
